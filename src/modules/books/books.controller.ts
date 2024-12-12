726e57e0b58543584158431624f2f9741e373583
jackson10fsa@gmail.com <jackson10fsa@gmail.com>
2024-12-12 19:00:21 -0300

    TL-08-Jackson

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-
+// books.controller.ts
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Book } from './books.entity';
 
 @Controller('books')
 export class BooksController {
-    constructor(private readonly booksService: BooksService){}
+    constructor(private readonly booksService: BooksService) {}
+
     @Get()
     findAll() {
         return this.booksService.findAll();
+    }
+
+    @Post()
+    async create(@Body() body: { id: number; title: string; year: number }) {
+        try {
+            const newBook = new Book(body.id, body.title, body.year);
+
+            const createdBook = await this.booksService.create(newBook);
+            return createdBook;
+
+        } catch (error) {
+            console.error(error);
+            throw error; //Passa o erro para o NestJS 
+        }
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() body: { title: string; year: number }) {
+        try {
+            const updatedBook = await this.booksService.update(id, body);
+
+            return updatedBook;
+
+        } catch (error) {
+            console.error(error);
+            throw error; //passa o erro para o Nest.JS 
+        }
     }
 }
